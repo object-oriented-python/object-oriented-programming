e4458dc740cc13dc1a3edd61f102666616858a50
David Ham <David.Ham@imperial.ac.uk>
2019-05-26 12:45:23 +0100

    Fix setup.py for oo module

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
 from glob import glob
 
-setup(name='finite-element-course',
+setup(name='oo-programming-exercises',
       version=2018.0,
-      description="""Skeleton finite element implementation in support of maths M345A47 at
-Imperial College London""",
+      description="""Code for Object oriented programming in Python for mathematicians.""",
       author="David Ham",
       author_email="david.ham@imperial.ac.uk",
-      url="https://finite-element.github.io/",
-      packages=["fe_utils"],
+      #url="https://finite-element.github.io/",
+      packages=[],
       scripts=glob('scripts/*'))
