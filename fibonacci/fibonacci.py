ec0ff1b2f61bb81b2ab6957af14e2d4e6852d4e6
David Ham <David.Ham@imperial.ac.uk>
2020-09-04 10:49:10 +0100

    Fix syntax errors

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,23 +1,24 @@
 from numbers import Integral
 
+
 def fib(n):
-       """Return the n-th Fibonacci number."""
-       if n == 0:
-           return 0
-       elif n == 1:
-           return 1
-       else:
-           return fib(n-2) + fib(n-1)
+    """Return the n-th Fibonacci number."""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-2) + fib(n-1)
+
 
 def typesafe_fib(n):
-       """Return the n-th Fibonacci number, raising an exception if a
-       non-integer is passed as n."""
-       if not isinstance(n, Integral):
-              raise TypeError
-                     f"fib expects an integer, not a {type(n).__name__}")
-       if n == 0:
-           return 0
-       elif n == 1:
-           return 1
-       else:
-           return fib(n-2) + fib(n-1)
+    """Return the n-th Fibonacci number, raising an exception if a
+    non-integer is passed as n."""
+    if not isinstance(n, Integral):
+        raise TypeError(f"fib expects an integer, not a {type(n).__name__}")
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-2) + fib(n-1)
