422a2883ad5f70a5a5567ad3b7c5c89c4fa24182
David Ham <David.Ham@imperial.ac.uk>
2021-03-07 11:32:34 +0000

    Basic numpoly test.

---
+++ tests/test_numpoly.py
@@ -0,0 +1,8 @@
+"""Basic proof of life check for numpoly."""
+
+def test_numpoly():
+    import numpoly
+    x = numpoly.symbols("x")
+    p = x**2 + 3*x + 1
+
+    assert numpoly.diff(p, x) == 2*x + 3
